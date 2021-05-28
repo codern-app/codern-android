5c917411f301e69c07b35fb83ddab7d9d2cc2870
Arman Kolahan <akolahan@gmail.com>
2021-05-28 21:37:35 +0200

    config

--- app/src/main/java/com/armanco/codern/di/AppModule.kt
+++ app/src/main/java/com/armanco/codern/di/AppModule.kt
@@ -6,6 +6,9 @@ import com.armanco.codern.data.db.Db
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,6 +24,17 @@ object AppModule {
     @Provides
     fun provideFirestore(): FirebaseFirestore {
         return Firebase.firestore
+    }
+
+    @Singleton
+    @Provides
+    fun provideRemoteConfig(): FirebaseRemoteConfig {
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        return remoteConfig
     }
 
     @Singleton
